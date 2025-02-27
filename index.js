c8fcc5b36805f040757f95420de233917407367c
malik rasheed <codedev366@gmail.com>
2025-02-28 00:11:32 +0500

    initialize backend project with Express, MongoDB, and product API

---
+++ index.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 8000;
+const mongodb_uri = process.env.MONGODB_URI;
+const cloudinary_name = process.env.CLOUDINARY_CLOUD_NAME;
+const cloudinary_api_key = process.env.CLOUDINARY_API_KEY;
+const cloudinary_api_secret = process.env.CLOUDINARY_API_SECRET;
+const products = require("./products.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("home route");
+});
+app.get("/api/products", (req, res) => {
+  const { year, make, model, type } = req.query;
+  let filteredProducts = products;
+  if (year) {
+    filteredProducts = filteredProducts.filter((value) => value.year == year);
+    // console.log(filteredProducts);
+  }
+  if (make) {
+    filteredProducts = filteredProducts.filter((value) => value.make == make);
+  }
+  if (model) {
+    filteredProducts = filteredProducts.filter((value) => value.model == model);
+  }
+  if (type) {
+    filteredProducts = filteredProducts.filter(
+      (value) => value.filter_type == type
+    );
+  }
+
+  res.json(filteredProducts);
+});
+app.get("/api/products/filter_values", (req, res) => {
+  const make = products.map((item) => item.make);
+
+  const year = products.map((item) => item.year);
+  const model = products.map((item) => item.model);
+  const type = products.map((item) => item.filter_type);
+
+  const values = {
+    year: year,
+    make: make,
+    model: model,
+    type: type,
+  };
+
+  res.json(values);
+});
+
+app.listen(port || 8000, () => {
+  console.log("server portrunning on ", port);
+});
